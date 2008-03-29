5533f24ae6be5fcde902fc7352382f244b0bb2a9
Tim Harding <tim.harding@gmail.com>
2008-03-29 21:16:33 +0000

    added the fqdn to the deploy
    git-svn-id: svn+ssh://panic.gatezero.org/Users/tim/svn/smarterfitter/trunk@89 f306cc4e-a879-4387-a808-bc7e4c52b210

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'mongrel_cluster/recipes'
 
 set :application, "smarterfitter"
-set :repository,  "svn+ssh://panic/Users/tim/svn/#{application}/trunk"
+set :repository,  "svn+ssh://panic.gatezero.org/Users/tim/svn/#{application}/trunk"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
