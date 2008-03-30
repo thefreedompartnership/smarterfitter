3b6651555ea6508aa27241cce2b12f52bada8914
Tim Harding <tim.harding@gmail.com>
2008-03-30 21:08:14 +0000

    Added a symlink to the deployment.
    git-svn-id: svn+ssh://panic.gatezero.org/Users/tim/svn/smarterfitter/trunk@100 f306cc4e-a879-4387-a808-bc7e4c52b210

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,18 +6,20 @@ ssh_options[:port] = 1022
 set :application, "smarterfitter"
 set :repository,  "svn+ssh://panic.gatezero.org/Users/tim/svn/#{application}/trunk"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/sf/#{application}"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
-
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
 role :app, "hosted.smarterfitter.com"
 role :web, "hosted.smarterfitter.com"
 role :db,  "hosted.smarterfitter.com", :primary => true
 
+namespace :deploy do
+  namespace :link do
+    desc "Link the forums and blog into the current directory."
+    task :blog, :roles => :app do
+      run "(cd #{release_path}/public && ln -s /u/apps/blog_smarterfitter/blog)"
+      run "(cd #{release_path}/public && ln -s /u/apps/forum_smarterfitter/forums)"
+    end
+  end
+end
+
+after 'deploy:symlink', 'deploy:link:blog'
